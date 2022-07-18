3c5f118a583361d9516d5427c1a8e41ee309f3c1
uzzzakov <mu0305012323@gmail.com>
2022-07-18 20:57:17 +0300

    add swagger

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HotelDb>(options => 
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
@@ -11,38 +13,65 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     using var scope = app.Services.CreateScope();
     var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
     db.Database.EnsureCreated();
 }
 
-app.MapGet("/hotels", async (IHotelRepository repository) => await repository.GetHotelsAsync());
+app.MapGet("/hotels", async (IHotelRepository repository) => 
+    Results.Ok(await repository.GetHotelsAsync()))
+    .Produces<List<Hotel>>(StatusCodes.Status200OK)
+    .WithName("GetAllHotels")
+    .WithTags("Getters");
 
 app.MapGet("/hotels/{id}", async (int id, IHotelRepository repository) => 
     await repository.GetHotelAsync(id) is Hotel hotel
     ?Results.Ok(hotel)
-    :Results.NotFound());
+    :Results.NotFound())
+    .Produces<Hotel>(StatusCodes.Status200OK)
+    .WithName("GetHotel")
+    .WithTags("Getters");
 
 app.MapPost("/hotels", async ([FromBody]Hotel hotel, IHotelRepository repository) => 
     {
         await repository.InsertHotelAsync(hotel);
         await repository.SaveAsync();
         return Results.Created($"/hotels/{hotel.Id}", hotel);
-    });
+    })
+    .Accepts<Hotel>("application/json")
+    .Produces<Hotel>(StatusCodes.Status201Created)
+    .WithName("CreateHotel")
+    .WithTags("Creators");
 
 app.MapPut("/hotels", async ([FromBody]Hotel hotel, IHotelRepository repository) => 
     {
         await repository.UpdateHotelAsync(hotel);
         await repository.SaveAsync();
         return Results.NoContent();
-    });
+    })
+    .Accepts<Hotel>("application/json")
+    .WithName("UpdateHotel")
+    .WithTags("Updaters");
 
 app.MapDelete("/hotels/{id}", async(int id, IHotelRepository repository) => 
     {
         await repository.DeleteHotelAsync(id);
         await repository.SaveAsync();
         return Results.NoContent();
-    });
+    })
+    .WithName("DeleteHotel")
+    .WithTags("Deleter");
+
+app.MapGet("/hotels/search/{name}", async(string name, IHotelRepository repository) =>
+    await repository.GetHotelsAsync(name) is IEnumerable<Hotel> hotels
+        ? Results.Ok(hotels)
+        : Results.NotFound(Array.Empty<Hotel>())) 
+        .Produces<List<Hotel>>(StatusCodes.Status200OK)
+        .WithName("GetHotelsByName")
+        .WithTags("Getters")
+        .ExcludeFromDescription();
 
 app.UseHttpsRedirection();
 
