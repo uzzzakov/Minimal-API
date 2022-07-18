4e503023cea045eff75b0e97d10c4f39a713a7bf
uzzzakov <mu0305012323@gmail.com>
2022-07-18 20:06:37 +0300

    add db, create crud

---
+++ Data/Hotel.cs
@@ -0,0 +1,8 @@
+public class Hotel 
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public int Stars { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,55 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelDb>(options => 
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<HotelDb>();
+    db.Database.EnsureCreated();
+}
+
+app.MapGet("/hotels", async (HotelDb db) => await db.Hotels.ToListAsync());
+
+app.MapGet("/hotels/{id}", async (int id, HotelDb db) => 
+    await db.Hotels.FirstOrDefaultAsync(h => h.Id == id) is Hotel hotel
+    ?Results.Ok(hotel)
+    :Results.NotFound());
+
+app.MapPost("/hotels", async ([FromBody]Hotel hotel, HotelDb db) => 
+    {
+        db.Hotels.Add(hotel);
+        await db.SaveChangesAsync();
+        return Results.Created($"/hotels/{hotel.Id}", hotel);
+    });
+
+app.MapPut("/hotels", async ([FromBody]Hotel hotel, HotelDb db) => 
+    {
+        var hotelFromDb = await db.Hotels.FindAsync(new object[] {hotel.Id});
+        if (hotelFromDb == null) return Results.NotFound();
+        hotelFromDb.Name = hotel.Name;
+        hotelFromDb.City = hotel.City;
+        hotelFromDb.Country = hotel.Country;
+        hotelFromDb.Stars = hotel.Stars;
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    });
+
+app.MapDelete("/hotels/{id}", async(int id, HotelDb db) => 
+    {
+        var hotelFromDb = await db.Hotels.FindAsync(new object[]{id});
+        if (hotelFromDb == null) return Results.NotFound();
+        db.Remove(hotelFromDb);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    });
+
+app.UseHttpsRedirection();
 
 app.Run();
+
